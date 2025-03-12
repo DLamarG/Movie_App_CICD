da5d31ac272ec618356cc40148bd50c0a000a846
DLamarG <demondg8@gmail.com>
2025-03-11 19:08:06 -0500

    changd

--- do-assessment3-movie-db/templates/flask-react/nginx/reactApp/src/App.jsx
+++ do-assessment3-movie-db/templates/flask-react/nginx/reactApp/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const API_URL = process.env.REACT_APP_API_URL;
+//const API_URL = process.env.REACT_APP_API_URL;
 
-// const API_URL = 'http://127.0.0.1:5500'
+const API_URL = 'http://127.0.0.1:5500'
 
 
 function App() {
